e6cbfb572d70dd4e8c08417bf1672cf62e296db8
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-10 10:19:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,6 @@
 def reformat_languages(languages)
 new_hash = {}
 languages.each do |style, language|
-  binding.pry
   language.each do |name, attributes|
   new_hash[name] ||= attributes
   new_hash[name][:style]=[]
