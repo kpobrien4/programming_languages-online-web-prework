122d455e234d711a8b8846aae6f2427b6072b778
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-10 10:22:37 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,13 +1,28 @@
 def reformat_languages(languages)
-new_hash = {}
-languages.each do |style, language|
-  language.each do |name, attributes|
-  new_hash[name] ||= attributes
-  new_hash[name][:style]=[]
-  new_hash[name][:style]<<style
+  new_languages = {}
+  languages.each do |style, language|
+    language.each do |name, attributes|
+    new_languages[name] ||= attributes
+    new_languages[name][:style]=[]
+    new_languages[name][:style]<<style
+    end
+  end
+  new_languages
 end
-end
-new_hash
+
+def reformat_languages(languages)
+  new_languages = {}
+  languages.each do |style, language|
+    language.each do |language_name, attribute|
+      if new_languages[language_name] == nil
+          new_languages[language_name] = attribute
+        end
+      new_languages[language_name][:style] = []
+      new_languages[language_name][:style] << style
+    end
+  end
+  new_languages[:javascript][:style] << :oo
+  new_languages
 end
 
 languages = {
@@ -17,8 +32,27 @@ languages = {
     },
     :javascript => {
       :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
     }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
   }
 }
-
-reformat_languages(languages)
